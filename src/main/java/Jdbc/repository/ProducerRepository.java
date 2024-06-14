e9614142e7f3f718b7ed22a2b278f50991c322df
Henrique <nuneshp2@gmail.com>
2024-06-13 23:20:58 -0300

    JDBC

--- src/main/java/Jdbc/repository/ProducerRepository.java
+++ src/main/java/Jdbc/repository/ProducerRepository.java
@@ -1,23 +1,75 @@
 package Jdbc.repository;
+
 import Jdbc.conn.ConnectionFactory;
 import Jdbc.dominio.Producer;
 import lombok.extern.log4j.Log4j2;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 @Log4j2
 
 public class ProducerRepository {
-    public static void save(Producer producer){
-        String sql = "INSERT INTO `anime_store`.`producer` (`name`) VALUES ('%');".formatted(producer.getName());
-        try(Connection coon = ConnectionFactory.getConnection();
-            Statement stm = coon.createStatement()){
+    public static void save(Producer producer) {
+        String sql = "INSERT INTO `anime_store`.`producer` (`name`) VALUES ('%s');".formatted(producer.getName());
+        try (Connection coon = ConnectionFactory.getConnection();
+             Statement stm = coon.createStatement()) {
             int rowsAffected = stm.executeUpdate(sql);
-            log.info("Inserted producer database, rows affected '{}'", rowsAffected);
-        }catch (SQLException e){
+            log.info("Inserted producer '{}' in the database, rows affected '{}'", producer.getName(), rowsAffected);
+        } catch (SQLException e) {
+            log.error("Error while trying to insert producer '{}'", producer.getName(), e);
             e.printStackTrace();
         }
-}
+    }
+
+    public static void delete(int id) {
+        String sql = "DELETE FROM `anime_store`.`producer` WHERE (`id` = '%d');".formatted(id);
+        try (Connection coon = ConnectionFactory.getConnection();
+             Statement stm = coon.createStatement()) {
+            int rowsAffected = stm.executeUpdate(sql);
+            log.info("Deleted producer '{}' from the database, rows affected '{}'", id, rowsAffected);
+        } catch (SQLException e) {
+            log.error("Error while trying to delete producer '{}'", id, e);
+        }
+    }
+
+    public static void update(Producer producer) {
+        String sql = "UPDATE `anime_store`.`producer` SET `name` = '%s' WHERE (`id` = '%d');\n".formatted(producer.getName(), producer.getId());
+        try (Connection coon = ConnectionFactory.getConnection();
+             Statement stm = coon.createStatement()) {
+            int rowsAffected = stm.executeUpdate(sql);
+            log.info("Update producer '{}', rows affected '{}'", producer.getId(), rowsAffected);
+        } catch (SQLException e) {
+            log.error("Error while trying to update producer '{}'", producer.getId(), e);
+        }
+    }
+
+    public static List<Producer> findAll() {
+        log.info("Finding all Producers");
+       return findByName("String");
+    }
+    public static List<Producer> findByName(String name) {
+        log.info("Finding Producers by name");
+        String sql = "SELECT * FROM anime_store.producer; where name like '%%%s%%';".formatted(name);
+        List<Producer> producers = new ArrayList<>();
+        try (Connection coon = ConnectionFactory.getConnection();
+             Statement stm = coon.createStatement();
+             ResultSet rs = stm.executeQuery(sql)) {
+            while (rs.next()) {
+                Producer producer = Producer
+                        .builder()
+                        .id(rs.getInt("id"))
+                        .name(rs.getString("name"))
+                        .build();
+                producers.add(producer);
+            }
+        } catch (SQLException e) {
+            log.error("Error while trying to find all producers", e);
+        }
+        return producers;
+    }
 }
---
+++ src/main/java/Jdbc/service/ProducerService.java
@@ -0,0 +1,30 @@
+package Jdbc.service;
+import Jdbc.dominio.Producer;
+import Jdbc.repository.ProducerRepository;
+
+import java.util.List;
+
+public class ProducerService {
+    public static void save(Producer producer){
+        ProducerRepository.save(producer);
+    }
+    public static void delete(Integer id) throws IllegalAccessException {
+        requireValidId(id);
+        ProducerRepository.delete(id);
+    }
+    public static void update(Producer producer) throws IllegalAccessException {
+        requireValidId(producer.getId());
+        ProducerRepository.update(producer);
+    }
+    public static List<Producer> findAll() throws IllegalAccessException {
+        return ProducerRepository.findAll();
+    }
+    public static List<Producer> findByName(String name) throws IllegalAccessException {
+        return ProducerRepository.findByName(name);
+    }
+    private static void requireValidId(Integer id) throws IllegalAccessException {
+        if (id == null || id <= 0){
+            throw new IllegalAccessException("Invalid value for id");
+        }
+    }
+}
